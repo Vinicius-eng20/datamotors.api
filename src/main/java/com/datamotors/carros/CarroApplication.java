09f69a61ccc3b2d8dd36dbdba596f7cb93d7a49c
Vinicius Silva <vinisilva5416@gmail.com>
2024-05-02 21:26:22 -0300

    corrigindo CORS

--- src/main/java/com/datamotors/carros/CarroApplication.java
+++ src/main/java/com/datamotors/carros/CarroApplication.java
@@ -2,6 +2,10 @@ package com.datamotors.carros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class CarroApplication {
@@ -9,5 +13,17 @@ public class CarroApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CarroApplication.class, args);
 	}
+
+	@Configuration
+    @EnableWebMvc
+    public class WebMvcConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("*")
+                    .allowedMethods("GET")
+                    .allowedHeaders("*");
+        }
+    }
 
 }
